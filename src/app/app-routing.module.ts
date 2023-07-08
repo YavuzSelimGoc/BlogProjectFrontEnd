de953f972d44348c6c92da86646d4c09a823a4a0
yavuzselimgoc <202804008@ogr.cbu.edu.tr>
2023-07-08 13:51:48 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicLayoutComponent } from './components/public-layout/public-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrivateLayoutComponent } from './components/private-layout/private-layout.component';
+import { IndexComponent } from './components/index/index.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: PublicLayoutComponent, children: [
+      {path:"",pathMatch:"full",component:IndexComponent},
+   
+    ]
+  },
+  {
+    path: "login", component: LoginComponent, children: [
+      { path: "", pathMatch: "full", component: LoginComponent },
+      { path: "login", component: LoginComponent },
+    ]
+  },
+  {
+    path: "admin", component: PrivateLayoutComponent, children: [
+     { path: "addCategory", pathMatch: "full", component: AddCategoryComponent },
+     
+ 
+    ]
+  }
+
+
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicLayoutComponent } from './components/public-layout/public-layout.component';
+import { PrivateLayoutComponent } from './components/private-layout/private-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { IndexComponent } from './components/index/index.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { UploadsComponent } from './components/uploads/uploads.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicLayoutComponent,
+    PrivateLayoutComponent,
+    LoginComponent,
+    IndexComponent,
+    AddCategoryComponent,
+    UploadsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
